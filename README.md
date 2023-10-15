1f1d7701be1d444c964952205cef81d974e51104
CG80499 <cg80492@gmail.com>
2023-10-15 01:45:48 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Factored-verification-dataset
-Paper dataset for "Factored Verification: Detecting and Reducing Hallucination in Summaries of Academic Papers"
+# Factored verification dataset
+Paper dataset for "Factored Verification: Detecting and Reducing Hallucination in Summaries of Academic Papers". 
+
+This repository contains the JSON file with 200 papers and queries used in the paper and a Python file to parse it.
---
+++ parse_data.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+import json
+
+# Models
+
+class Paper(BaseModel):
+    authors: list[str]
+    summary: str
+    title: str
+
+class Sample(BaseModel):
+    papers: list[Paper]
+    query: str
+
+with open('paper_dataset.json') as f:
+    raw_data = json.load(f)
+    data = [Sample.parse_obj(d) for d in raw_data]
+
+print("First query: ", data[0].query)
+print("First paper of first query: ", data[0].papers[0].title)
